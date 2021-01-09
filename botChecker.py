06464c4df3264efae2f81a0134523401323b0a40
wiwich <wiwich0401@gmail.com>
2021-01-09 21:38:05 +0700

    Add server time and coins
    - add server time and convert to local time
    - add coins

--- botChecker.py
+++ botChecker.py
@@ -1,5 +1,5 @@
 ### botcheckerTest.py
-### Modified: 8 Jan 2021
+### Modified: 9 Jan 2021
 ###
 ### Line bot checker
 ###
@@ -15,59 +15,64 @@ from songline import Sendline
 
 ### ========= function ========= 
 
-def checkCondition(sym,ticker):
-    global btcValue
-    cons = 10000 # constant condition
-    last = ticker[sym]['last']
-    highestbid = ticker[sym]['highestBid']
-    lowerstask = ticker[sym]['lowestAsk']
-    
+def checkCondition(ticker):
+    global value_list
+    sym_list = ['THB_BTC','THB_ETH','THB_USDT','THB_BNB','THB_BAND','THB_DOGE']
+    cons_list = [10000,1000,1,100,50,0.1] # set constant variable to be conditions
+     
     text = ''
     
-    if last >= btcValue + cons:
-        #txt = 'btcValue: {:,.3f}, last: {:,.3f}'.format(btcValue,last)
-        #text += txt + '\n'
-        txt = '{} ราคาขึ้น {:,.3f} \n(highest bid: {:,.3f},lowest ask: {:,.3f})'.format(sym,last,highestbid,lowerstask)
-        btcValue = last
-        text += txt + '\n'
-        print(text)
-    elif last <= btcValue - cons:
-        #txt = 'btcValue: {:,.3f}, last: {:,.3f}'.format(btcValue,last)
-        #text += txt + '\n'
-        txt = '{} ราคาลง {:,.3f} \n(highest bid: {:,.3f},lowest ask: {:,.3f})'.format(sym,last,highestbid,lowerstask)
-        btcValue = last
-        text += txt + '\n'
-        print(text)
-
-    print(last)
+    for i in range(len(sym_list)):
+        sym = sym_list[i]
+        cons = cons_list[i]
+        value = value_list[i]
+        
+        last = ticker[sym]['last']
+        highestbid = ticker[sym]['highestBid']
+        lowerstask = ticker[sym]['lowestAsk']
+        if last >= value + cons:
+            #txt = 'btcValue: {:,.3f}, last: {:,.3f}'.format(value[i],last)
+            #text += txt + '\n'
+            txt = '>> {} ราคาขึ้น {:,.3f} \n(highest bid: {:,.3f},lowest ask: {:,.3f})'.format(sym,last,highestbid,lowerstask)
+            value[i] = last
+            text += txt + '\n'
+            print(text)
+        elif last <= value - cons:
+            #txt = 'btcValue: {:,.3f}, last: {:,.3f}'.format(value[i],last)
+            #text += txt + '\n'
+            txt = '>> {} ราคาลง {:,.3f} \n(highest bid: {:,.3f},lowest ask: {:,.3f})'.format(sym,last,highestbid,lowerstask)
+            value[i] = last
+            text += txt + '\n'
+           
+        print('{}: {:,.3f}'.format(sym,last))
+    print('-------')
     return text
 
 
 def checkPrice():
-    global btcValue
-    global btc
-
     textLine = ''
     
     marketTicker = requests.get(API_HOST+'/api/market/ticker') # all coin in board
     ticker = marketTicker.json() # change to dictionary(json)
 
-    checktext = checkCondition(btc,ticker)
+    marketEpoch = requests.get(API_HOST+'/api/servertime') # get server time (epoch time)
+    epoch = marketEpoch.json()
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))) # convert server time to local time
+    
+    checktext = checkCondition(ticker)
     if len(checktext)>0:
         textLine += checktext
         messenger.sendtext(textLine)
 
-    time.sleep(0.2)
+    time.sleep(0.5) # 0.5sec
     checkPrice()
 
 
 ### ========= main =========
-
 API_HOST = 'https://api.bitkub.com' # base URL
 
-token = tkn.TOKEN # line to me
+token = tkn.MY_TOKEN # line to me
 messenger = Sendline(token) # send message
-
 
 btc = 'THB_BTC'
 eth = 'THB_ETH'
@@ -75,11 +80,17 @@ eth = 'THB_ETH'
 marketTicker = requests.get(API_HOST+'/api/market/ticker') # all coin in board
 ticker = marketTicker.json() # change to dictionary(json)
 
-
 btcValue = ticker[btc]['last']
 ethValue = ticker[eth]['last']
+usdtValue = ticker['THB_USDT']['last']
+bnbValue = ticker['THB_BNB']['last']
+bandValue = ticker['THB_BAND']['last']
+dogeValue = ticker['THB_DOGE']['last']
+value_list = [btcValue,ethValue,usdtValue,bnbValue,bandValue,dogeValue]
 
 print('BTC:{:,.3f}'.format(btcValue))
+print('ETH:{:,.3f}'.format(ethValue))
+print('-----------')
 
 # print(ticker[sym]['last']) # print all coint
 
