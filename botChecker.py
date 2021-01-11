345a87e9cb54fff89f325ed0a5a5f63b7f8e2131
wiwich <wiwich0401@gmail.com>
2021-01-11 22:14:27 +0700

    change conditions: value to percentage

--- botChecker.py
+++ botChecker.py
@@ -10,6 +10,8 @@
 import requests
 import time
 import tkn
+import sys
+from datetime import datetime
 from songline import Sendline
 
 
@@ -17,34 +19,39 @@ from songline import Sendline
 
 def checkCondition(ticker):
     global value_list
-    sym_list = ['THB_BTC','THB_ETH','THB_USDT','THB_BNB','THB_BAND','THB_DOGE']
-    cons_list = [10000,1000,1,100,50,0.1] # set constant variable to be conditions
-     
+    # sym_list = ['THB_BTC','THB_ETH','THB_USDT','THB_BNB','THB_BAND','THB_DOGE']
+    sym_list = ['THB_BTC']
     text = ''
     
+    print('now: ',datetime.now())
     for i in range(len(sym_list)):
-        sym = sym_list[i]
-        cons = cons_list[i]
-        value = value_list[i]
         
-        last = ticker[sym]['last']
+        sym = sym_list[i]
+        cons = 1 # 1%        
+        value = value_list[i]
+        last = ticker[sym ]['last']
         highestbid = ticker[sym]['highestBid']
         lowerstask = ticker[sym]['lowestAsk']
-        if last >= value + cons:
+        if i==1:
+            print('BTC value: ',value) 
+        if last >= value + (value*cons/100):
+            print('percentage: ',value + (value*cons/100))
             #txt = 'btcValue: {:,.3f}, last: {:,.3f}'.format(value[i],last)
             #text += txt + '\n'
             txt = '>> {} ราคาขึ้น {:,.3f} \n(highest bid: {:,.3f},lowest ask: {:,.3f})'.format(sym,last,highestbid,lowerstask)
-            value[i] = last
+            value_list[i] = last
             text += txt + '\n'
             print(text)
-        elif last <= value - cons:
+        elif last <= value - (value*cons/100):
+            print('percentage: ',value - (value*cons/100))
             #txt = 'btcValue: {:,.3f}, last: {:,.3f}'.format(value[i],last)
             #text += txt + '\n'
             txt = '>> {} ราคาลง {:,.3f} \n(highest bid: {:,.3f},lowest ask: {:,.3f})'.format(sym,last,highestbid,lowerstask)
-            value[i] = last
+            value_list[i] = last
             text += txt + '\n'
            
         print('{}: {:,.3f}'.format(sym,last))
+        print(text)
     print('-------')
     return text
 
@@ -55,9 +62,9 @@ def checkPrice():
     marketTicker = requests.get(API_HOST+'/api/market/ticker') # all coin in board
     ticker = marketTicker.json() # change to dictionary(json)
 
-    marketEpoch = requests.get(API_HOST+'/api/servertime') # get server time (epoch time)
-    epoch = marketEpoch.json()
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))) # convert server time to local time
+    # marketEpoch = requests.get(API_HOST+'/api/servertime') # get server time (epoch time)
+    # epoch = marketEpoch.json()
+    # print('server time: ',time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))) # convert server time to local time
     
     checktext = checkCondition(ticker)
     if len(checktext)>0:
@@ -71,7 +78,7 @@ def checkPrice():
 ### ========= main =========
 API_HOST = 'https://api.bitkub.com' # base URL
 
-token = tkn.MY_TOKEN # line to me
+token = tkn.TOKEN # line to me
 messenger = Sendline(token) # send message
 
 btc = 'THB_BTC'
@@ -86,8 +93,8 @@ usdtValue = ticker['THB_USDT']['last']
 bnbValue = ticker['THB_BNB']['last']
 bandValue = ticker['THB_BAND']['last']
 dogeValue = ticker['THB_DOGE']['last']
-value_list = [btcValue,ethValue,usdtValue,bnbValue,bandValue,dogeValue]
-
+# value_list = [btcValue,ethValue,usdtValue,bnbValue,bandValue,dogeValue]
+value_list = [btcValue]
 print('BTC:{:,.3f}'.format(btcValue))
 print('ETH:{:,.3f}'.format(ethValue))
 print('-----------')
